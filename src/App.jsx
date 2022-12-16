ce5ca1a54f1297d301d3a5d64402f9ad23fa53e8
dausuke <ksye616@gmail.com>
2022-12-16 18:12:16 +0900

    ディクトリ変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import {Icon, NaviIcon} from './components/atoms';
+
 function App() {
   return (
     <div className="App">
       <h1>Twitter Trace</h1>
+      <Icon.BackIcon />
+      <NaviIcon.HomeIcon />
     </div>
   );
 }
--- src/assets/icon/index.js
+++
@@ -1,2 +0,0 @@
-export * as Icon from './Icon';
-export * as NaviIcon from './navi/NaviIcon';
