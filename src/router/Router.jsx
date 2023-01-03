1a30c575f2de08f1230979110fb45683fe1f11c8
dausuke <ksye616@gmail.com>
2023-01-03 16:38:29 +0900

    ユーザー画面作成

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,6 +3,7 @@ import {Feed} from '@/features/feed/pages/Feed';
 import {PostTweet} from '@/features/post/pages/PostTweet';
 import {Search} from '@/features/search/pages/Search';
 import {Notification} from '@/features/notification/pages/Notification';
+import {User} from '@/features/users/page/User';
 
 const createRoutes = element => createBrowserRouter(createRoutesFromElements(element));
 
@@ -11,6 +12,7 @@ const routes = createRoutes(
     <Route index element={<Feed />} />
     <Route path="search" element={<Search />} />
     <Route path="notification" element={<Notification />} />
+    <Route path="user/:accountName" element={<User />} />
     <Route path="post/tweet" element={<PostTweet />} />
   </Route>,
 );
