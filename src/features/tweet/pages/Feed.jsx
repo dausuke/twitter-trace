0648b2c8891adb6f594575010d30f1413b7c1a6d
dausuke <ksye616@gmail.com>
2023-01-17 01:33:16 +0900

    無限スクロール追加

--- src/features/tweet/pages/Feed.jsx
+++ src/features/tweet/pages/Feed.jsx
@@ -1,12 +1,37 @@
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import {css} from '@emotion/react';
-import {AddTweetButton, TweetItem, NaviInPage, HeaderAvator} from '@/common/components/parts';
-import {Box} from '@/common/components/atoms';
+import {AddTweetButton, TweetItem, NaviInPage, HeaderAvator} from '@/components/parts';
+import {Box} from '@/components/atoms';
 import Mock from '@/features/users/mock';
+import useInfinityScroll from '@/hooks/useInfinityScroll';
 import {getTweets} from '../api';
 
 export const Feed = () => {
+  const limit = 20;
   const [tweets, setTweets] = useState([]);
+  const [scrollRef, _, setPaged, isLoading, setIsLoading] = useInfinityScroll(async currentPage => {
+    try {
+      if (isLoading) return;
+
+      setIsLoading(true);
+
+      const params = {
+        limit,
+        paged: currentPage,
+      };
+      const response = await getTweets(params);
+
+      if (response.data.length < limit) setPaged(0);
+
+      const newItems = currentPage === 1 ? response.data : [...tweets, ...response.data];
+      setTweets(newItems);
+    } catch (e) {
+      console.error(e);
+      alert('ツイートを読み込めませんでした');
+    } finally {
+      setIsLoading(false);
+    }
+  });
 
   const headerOption = {
     headerLeft: <HeaderAvator user={Mock.me} />,
@@ -16,33 +41,13 @@ export const Feed = () => {
     },
   };
 
-  const onFetchTweets = async () => {
-    try {
-      const response = await getTweets();
-      setTweets(response.data);
-    } catch (e) {
-      console.error(e);
-      alert('ツイートを読み込めませんでした');
-    }
-  };
-
-  useEffect(() => {
-    onFetchTweets();
-  }, []);
-
-  useEffect(() => {
-    if (!!tweets.length) {
-      const container = document.getElementById('feedContainer');
-      container.scrollIntoView(false);
-    }
-  }, [tweets]);
-
   return (
     <NaviInPage headerOption={headerOption}>
-      <Box css={content} id="feedContainer">
+      <Box css={content}>
         {tweets.map((data, index) => (
           <TweetItem item={data} key={index} />
         ))}
+        <div ref={scrollRef} />
         <AddTweetButton />
       </Box>
     </NaviInPage>
