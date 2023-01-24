67758f37de8b8c5f70b9935812694f89e22a4488
dausuke <ksye616@gmail.com>
2023-01-24 23:58:34 +0900

    デフォルトアバター追加

--- src/components/atoms/element/Avator.jsx
+++ src/components/atoms/element/Avator.jsx
@@ -1,10 +1,11 @@
 import {css} from '@emotion/react';
 import {Colors} from '@/assets/styles';
 import {Image} from './Image';
+import {DefaultAvator} from '@/assets/defaultAvator';
 
 export const Avator = ({image, size = 90}) => (
   <div css={[avator, {width: size, height: size}]}>
-    <Image src={image} css={avatorImage} />
+    <Image src={image ?? DefaultAvator} css={avatorImage} />
   </div>
 );
 
