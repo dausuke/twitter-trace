231fec320a6ee6ca761a7ee5a15e2358d6a894f8
dausuke <ksye616@gmail.com>
2023-01-26 14:23:42 +0900

    設定にコメント追加

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // yarn devで自動的にブラウザ開く
   server: {
     open: true,
   },
+  // パスエイリアスの設定
+  // src以下を@で記載できる
   resolve: {
     alias: {
       '@/': `${__dirname}/src/`,
     },
   },
+  // emotionを使うときに必要な設定
   plugins: [react({jsxImportSource: '@emotion/react'})],
 });
